60f043f27f9d295e432cd5116eed1c6f0b1c896a
itheyma <itheyma@163.com>
2023-02-11 02:48:11 +0800

    first commit

---
+++ src/main/java/com/iflyy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.iflyy.git;
+
+/**
+ * @Author: iFlyy
+ * 2023/2/11 2:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
